7a647afb6fd7fe25347c3fcf5ccc54092f22f8bf
ravi4all <ravikanttyagi.2295@gmail.com>
2018-07-02 17:13:05 +0530

    Add files via upload

--- Applications/FB/main.py
+++ Applications/FB/main.py
@@ -1,23 +1,52 @@
+from datetime import  datetime
+
 users = []
 userData = {}
 
-def postSomething():
+posts = []
+postData = {}
+
+def postSomething(username):
     post = input("Enter your post : ")
     # [{'post':'Hello this is my post', 'date':6-29-2018, 'username':'Ram'}]
+    postData['post'] = post
+    postData['username'] = username
+    postData['date'] = datetime.now().date()
 
-def viewProfile():
+    posts.append(postData.copy())
+    for p in posts:
+        print(p)
+
+    loginSuccess(username)
+
+def viewProfile(username):
+    for user in users:
+        if user['name'] == username:
+            print("Email ID :",user['email'])
+    for p in posts:
+        if p['username'] == username:
+            print("Post :",p['post'])
+            print("Date :",p['date'])
+
+    loginSuccess(username)
+
+def updateProfile(username):
+    toUpdate = input("What do you want to update ? ")
+    updatedValue = input("Enter updated {}".format(toUpdate))
+
+    for user in users:
+        if user['name'] == username:
+            user[toUpdate] = updatedValue
+
+    loginSuccess(username)
+
+def deleteProfile(username):
     pass
 
-def updateProfile():
+def logout(username):
     pass
 
-def deleteProfile():
-    pass
-
-def logout():
-    pass
-
-def loginSuccess():
+def loginSuccess(username):
     print("""
     1. Post Something
     2. View Profile
@@ -25,6 +54,15 @@ def loginSuccess():
     4. Delete Profile
     5. Logout
     """)
+    userinput = input("Enter your choice : ")
+    operations = {
+        "1" : postSomething,
+        "2" : viewProfile,
+        "3" : updateProfile,
+        "4" : deleteProfile,
+        "5" : logout
+    }
+    operations.get(userinput, errorHandler)(username)
 
 def login():
     email = input("Enter your emailId : ")
@@ -33,7 +71,7 @@ def login():
     for user in users:
         if user['email'] == email and user['password'] == pwd:
             print("Login Success")
-            loginSuccess()
+            loginSuccess(user['name'])
             break
     else:
         print("Login Failed...")
@@ -48,7 +86,7 @@ def register():
     for user in users:
         print(user)
 
-def errorHandler():
+def errorHandler(*args):
     print("Wrong Choice...")
 
 def main():
